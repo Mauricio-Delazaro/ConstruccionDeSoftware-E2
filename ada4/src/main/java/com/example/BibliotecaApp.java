6a993c01bb4fd6e59eda9c4d14ef46794bc45bcd
Mauricio De lazaro <a18001025@alumnos.uady.mx>
2026-09-10 18:27:39 -0600

    cambiosfinales

--- ada4/src/main/java/com/example/BibliotecaApp.java
+++ ada4/src/main/java/com/example/BibliotecaApp.java
@@ -5,6 +5,15 @@ public class BibliotecaApp
 {
     public static void main( String[] args )
     {
-        
+        Usuario usuario = new Usuario('Agustin', 'agustincorreo@gmail.com', 5);
+        Libro libro = new Libro('Construccion', 'Daniel', '1235-abc', 2026);
+        Prestamo prestamo = new Prestamo('12-05-20', '12-07-20', 500, 5);
+
+        usuario.mostrarUsuario();
+        libro.mostrarLibro();
+        prestamo.calcularMulta()
+        prestamo.aplicarMulta();
+        prestamo.mostrarPrestamo();
+
     }
 }
